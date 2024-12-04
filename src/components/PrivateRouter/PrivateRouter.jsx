a447e9a872c22736292b3c7a2e361fc2a446b58e
Rakib <rh.rakibhasan365@gmail.com>
2024-12-04 11:23:57 +0600

    update

--- src/components/PrivateRouter/PrivateRouter.jsx
+++ src/components/PrivateRouter/PrivateRouter.jsx
@@ -1,11 +1,10 @@
 import { useContext } from "react";
 import { UserContext } from "../../provider/UserProvider/UserProvider";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const PrivateRouter = ({children}) => {
 
     const {user, loading} = useContext(UserContext);
-    const location = useLocation();
 
     if(loading){
         return <span className="loading loading-spinner loading-lg"></span>
@@ -14,7 +13,7 @@ const PrivateRouter = ({children}) => {
         return children;
     }
 
-    return <Navigate state={location.pathname} to="/login"></Navigate>
+    return <Navigate to="/login"></Navigate>
 };
 
 export default PrivateRouter;
