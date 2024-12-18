a45c6545c625056526e65c5cf2249ce155f2423c
Rakib <rh.rakibhasan365@gmail.com>
2024-12-18 13:02:03 +0600

    another issue solved

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,8 +5,8 @@ const Banner = () => {
     return (
         <div className="flex flex-col-reverse lg:flex-row gap-10 items-center lg:mt-20 lg:mb-6 mb-8 lg:justify-between">
             <div className="text-left lg:w-1/2">
-                <p className="lg:text-6xl text-4xl font-bold mb-4">One Step Closer <br /> <span className="text-orange-500">To</span> <br /> Your <span className=" text-blue-500">Dream Job</span> </p>
-                <p className="text-justify hyphens-auto leading-relaxed text-black lg:text-lg">Career Hub is your go-to destination for innovative IT solutions. We specialize in software development, cloud computing, web and app development, and IT consulting. Our mission is to empower businesses with cutting-edge tools, streamlined processes, and tailored strategies to drive growth and success. At Career Hub, we turn technology into opportunities.</p>
+                <p className="lg:text-6xl md:text-4xl text-3xl font-bold mb-4">One Step Closer To <br /> <span className=" text-blue-600">Your Dream Job</span> </p>
+                <p className="text-justify hyphens-auto leading-relaxed text-black lg:text-lg mt-6">Career Hub is your go-to destination for innovative IT solutions. We specialize in software development, cloud computing, web and app development, and IT consulting. Our mission is to empower businesses with cutting-edge tools, streamlined processes, and tailored strategies to drive growth and success. At Career Hub, we turn technology into opportunities.</p>
                 <div>
                     <Link to="/register"><button className="mt-6 bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900 hover:from-blue-700 hover:to-blue-500 hover:text-black px-8 py-4 pt-2 pb-2 rounded-md  text-white font-semibold sm:mb-8 sm:pt-2">Get Started</button></Link>
                 </div>
--- src/components/CategoryBanner/CategoryBanner.jsx
+++ src/components/CategoryBanner/CategoryBanner.jsx
@@ -6,7 +6,7 @@ const CategoryBanner = () => {
             <div className="hero-content text-center">
                 <div className="bg-opacity-70 flex items-center lg:h-[250px] lg:w-3/4 bg-black px-4 lg:px-0">
                     <div className='lg:px-24 py-6 lg:py-0'>
-                        <h1 className="mb-5 lg:text-5xl text-4xl font-bold uppercase text-blue-600">Job Category</h1>
+                        <h1 className="mb-5 lg:text-5xl text-3xl font-bold uppercase text-blue-600">Job Category</h1>
                         <p className="mb-5 text-white text-justify leading-relaxed break-word hyphens-auto">
                         To build a successful career, concentrate on developing relevant skills, setting clear goals, and engaging in continuous learning. Begin by identifying a job that aligns with your interests and values. Then, work diligently to gain experience and expertise in that role. Stay adaptable and look for opportunities for growth, ensuring that each step you take brings you closer to your long-term goals.
                         </p>
