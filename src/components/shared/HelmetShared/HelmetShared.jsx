dff898b0c1b263edb8d1860f3919be07b89597d3
Rakib <rh.rakibhasan365@gmail.com>
2024-12-30 01:57:04 +0600

    title update

--- src/components/shared/HelmetShared/HelmetShared.jsx
+++ src/components/shared/HelmetShared/HelmetShared.jsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 const HelmetShared = ({helmetTitle}) => {
     return (
         <div>
-            <Helmet><title>Tech Hub - { helmetTitle}</title></Helmet>
+            <Helmet><title>Career Hub - { helmetTitle}</title></Helmet>
         </div>
     );
 };
